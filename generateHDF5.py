a4d33893658abb292d630797b31ab525535a0499
Mr.Ye <Owen718@users.noreply.github.com>
2022-04-25 23:40:24 +0800

    update

--- generateHDF5.py
+++ generateHDF5.py
@@ -19,30 +19,34 @@ def process(f):
 prefix = '/HOME/scz0088/run/datasets/imagenet-mini/train/'
 l = list(map(lambda x : os.path.join(prefix, x), os.listdir(prefix)))
 
-i = 0
-imagenet = np.zeros((len(l), image_size, image_size, 3), dtype='uint8')
-pool = multiprocessing.Pool(num_cpus)
-while i < len(l):
-    current_batch = l[i:i + batch_size]    
-    current_res = np.array(pool.map(process, current_batch))
-    imagenet[i:i + batch_size] = current_res    
-    i += batch_size
-    print(i, 'images')
-    
-prefix = '/HOME/scz0088/run/datasets/imagenet-mini/val/'
-l_val = list(map(lambda x : os.path.join(prefix, x), os.listdir(prefix)))
+print(l)
 
-i = 0
-imagenet_val = np.zeros((len(l_val), image_size, image_size, 3), dtype='uint8')
-pool = multiprocessing.Pool(multiprocessing.cpu_count())
 
-while i < len(l_val):
-    current_batch = l_val[i:i + batch_size]    
-    current_res = np.array(pool.map(process, current_batch))
-    imagenet_val[i:i + batch_size] = current_res    
-    i += batch_size
-    print(i, 'images')
+
+# i = 0
+# imagenet = np.zeros((len(l), image_size, image_size, 3), dtype='uint8')
+# pool = multiprocessing.Pool(num_cpus)
+# while i < len(l):
+#     current_batch = l[i:i + batch_size]    
+#     current_res = np.array(pool.map(process, current_batch))
+#     imagenet[i:i + batch_size] = current_res    
+#     i += batch_size
+#     print(i, 'images')
     
-with h5py.File('/HOME/scz0088/run/datasets/hdf5/imagenet_mini-128.hdf5', 'w') as f:
-    f['train'] = imagenet
-    f['val'] = imagenet_val
+# prefix = '/HOME/scz0088/run/datasets/imagenet-mini/val/'
+# l_val = list(map(lambda x : os.path.join(prefix, x), os.listdir(prefix)))
+
+# i = 0
+# imagenet_val = np.zeros((len(l_val), image_size, image_size, 3), dtype='uint8')
+# pool = multiprocessing.Pool(multiprocessing.cpu_count())
+
+# while i < len(l_val):
+#     current_batch = l_val[i:i + batch_size]    
+#     current_res = np.array(pool.map(process, current_batch))
+#     imagenet_val[i:i + batch_size] = current_res    
+#     i += batch_size
+#     print(i, 'images')
+    
+# with h5py.File('/HOME/scz0088/run/datasets/hdf5/imagenet_mini-128.hdf5', 'w') as f:
+#     f['train'] = imagenet
+#     f['val'] = imagenet_val
